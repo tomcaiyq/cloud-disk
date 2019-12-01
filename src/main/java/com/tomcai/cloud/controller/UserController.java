d9be1ce1b65679c1ddb26fa110b6305ae9b39f2b
tomcaiyq <1348870256@qq.com>
2019-12-01 13:08:48 +0800

    1.3

--- src/main/java/com/tomcai/cloud/controller/UserController.java
+++ src/main/java/com/tomcai/cloud/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 @Controller
 public class UserController {
@@ -22,6 +23,9 @@ public class UserController {
 
     @RequestMapping("/login")
     public String login() {
+        if (!Objects.isNull(SecurityUtils.getSubject().getSession().getAttribute("user"))) {
+            return "redirect:home";
+        }
         return "login";
     }
 
