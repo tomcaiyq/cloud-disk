5a3573b6ed99725568f55271ae8fb6a05a4d8d57
tomcaiyq <1348870256@qq.com>
2019-12-15 21:34:13 +0800

    1.4

--- src/main/java/com/tomcai/cloud/interceptor/AccessInterceptor.java
+++ src/main/java/com/tomcai/cloud/interceptor/AccessInterceptor.java
@@ -10,7 +10,35 @@ import javax.servlet.http.HttpServletResponse;
 public class AccessInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        log.info("{}{}", request.getRemoteHost(), request.getRequestURI());
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_CLIENT_IP");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getRemoteAddr();
+            }
+        } else if (ip.length() > 15) {
+            String[] ips = ip.split(",");
+            for (String s : ips) {
+                if (!("unknown".equalsIgnoreCase(s))) {
+                    ip = s;
+                    break;
+                }
+            }
+        }
+
+        log.info("{}{}", ip, request.getRequestURI());
         log.info(request.getHeader("User-Agent"));
         return true;
     }
