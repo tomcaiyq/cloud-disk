5fb9c485b53bb0d0c4379caf35c23b29cadfeb63
tomcaiyq <1348870256@qq.com>
2019-12-18 15:37:01 +0800

    1.4

--- src/main/java/com/tomcai/cloud/interceptor/AccessInterceptor.java
+++ src/main/java/com/tomcai/cloud/interceptor/AccessInterceptor.java
@@ -10,7 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 public class AccessInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        log.info("{}{}", request.getRemoteHost(), request.getRequestURI());
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        log.info("{}{}", ip, request.getRequestURI());
         log.info(request.getHeader("User-Agent"));
         return true;
     }
